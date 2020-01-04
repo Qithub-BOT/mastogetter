c65be1c059646d9b5804fac43743054360bafe6d
hidao80 <8155294+hidao80@users.noreply.github.com>
2020-01-04 22:37:22 +0900

    fix: decodePearmalink() correspondence.

--- js/p.js
+++ js/p.js
@@ -1,19 +1,8 @@
 function showPreview() {
-	let instance_full = get_url_vars["i"];
-	if (instance_full.trim() == "") {
-		instance_full = "https://qiitadon.com";
-		$("instance").value = instance_full;
-	}
-	let instance = instance_full.split("//")[1];
-	let toot_id = get_url_vars["t"];
-	let toot_ids = toot_id.split(',');
-	if (toot_ids[toot_ids.length-1] < "1000000000000000") {
-		// 最後の要素が 1.0+E18より小さければ、
-		// id の途中で url が切れたと判断して最後の項目を
-		// 除外（仕様）
-		toot_ids.pop();
-	}
-
+	let permalink = decodePermalink(get_url_vars);
+	let instance_full = permalink["instance_full"];
+	let instance = permalink["instance"];
+	let toot_ids = permalink["toot_ids"];
 	let toot_url = "";
 	let target_div = $("cards");
 	let xhr = new XMLHttpRequest();
