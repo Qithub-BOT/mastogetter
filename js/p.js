8d52eeb2dfc7b2bba9804e923669a18528bf412e
hidao <8155294+hidao80@users.noreply.github.com>
2020-01-04 18:20:28 +0900

    Update p.js

--- js/p.js
+++ js/p.js
@@ -44,14 +44,15 @@ function showPreview() {
 					let obj = JSON.parse(xhr.responseText);
 					let e = document.createElement("div");
 					e.setAttribute("class","toot");
+					let tmp = "";
+					for (let i = 0; i < obj.media_attachments.length; i++) {
+						tmp += "<a href='"+ obj.media_attachments[i].url +"'><img class='thumbs' src='"+ obj.media_attachments[i].preview_url +"'></a>";
+					}
 					e.innerHTML = '<div class="box"><img width="48" height="48" alt="" class="u-photo" src="'+ obj.account.avatar +'"></div>'
 						+ '<div class="box"><span class="display-name">'+ obj.account.display_name
 						+ '<span>@'+ obj.account.username +'@'+ instance +'</span></span>'
-						+ '<div class="e-content" lang="ja" style="display: block; direction: ltr"><p>'+ obj.content +'</p></div>';
-					for (let i = 0; i < obj.media_attachments.length; i++) {
-						target_div.innerHTML += "<a href='"+ obj.media_attachments[i].url +"'><img class='thumbs' src='"+ obj.media_attachments[i].preview_url +"'></a>";
-					}
-					e.innerHTML += '</div>';
+						+ '<div class="e-content" lang="ja" style="display: block; direction: ltr"><p>'+ obj.content +'</p></div>'
+						+ tmp + '</div>';
 					target_div.appendChild(e);
 				} else {
 					console.error(xhr.statusText);
