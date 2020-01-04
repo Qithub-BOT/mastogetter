387d21fa3440574b47994267ac1f629dc0b5c733
hidao80 <8155294+hidao80@users.noreply.github.com>
2020-01-04 22:27:13 +0900

    fix: import common.js.

--- js/p.js
+++ js/p.js
@@ -1,20 +1,3 @@
-function $(id) {
-	return document.getElementById(id);
-}
-
-var get_url_vars = (function() {
-	var vars = {};
-	var param = location.search.substring(1).split('&');
-	for(var i = 0; i < param.length; i++) {
-			var keySearch = param[i].search(/=/);
-			var key = '';
-			if(keySearch != -1) key = param[i].slice(0, keySearch);
-			var val = param[i].slice(param[i].indexOf('=', 0) + 1);
-			if(key != '') vars[key] = decodeURI(val);
-	}
-	return vars;
-})();
-
 function showPreview() {
 	let instance_full = get_url_vars["i"];
 	if (instance_full.trim() == "") {
