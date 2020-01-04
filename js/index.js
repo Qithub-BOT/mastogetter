f032d369938081953c1c9957b7366bb44aa5fae2
hidao <8155294+hidao80@users.noreply.github.com>
2020-01-04 17:58:58 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,7 +25,11 @@ function showPreview() {
 				target_div.innerHTML = '<div class="toot"><div class="box"><img width="48" height="48" alt="" class="u-photo" src="'+ obj.account.avatar +'"></div>'
 					+ '<div class="box"><span class="display-name">'+ obj.account.display_name
 					+ '<span>@'+ obj.account.username +'@'+ instance +'</span></span>'
-					+ '<div class="e-content" lang="ja" style="display: block; direction: ltr"><p>'+ obj.content +'</p></div></div></div>';
+					+ '<div class="e-content" lang="ja" style="display: block; direction: ltr"><p>'+ obj.content +'</p></div></div>';
+				for (let i = 0; i < obj.media_attachments.length; i++) {
+					target_div.innerHTML += "<a href='"+ obj.media_attachments[i].url +"'><img class='thumbs' src='"+ obj.media_attachments[i].preview_url +"'></a>";
+				}
+				target_div.innerHTML += '</div>';
 			} else {
 				console.error(xhr.statusText);
 			}
