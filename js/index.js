c6a33081d0d143f851003ef68685dbcf16635228
hidao <8155294+hidao80@users.noreply.github.com>
2020-01-04 13:44:12 +0900

    ajax対応

--- js/index.js
+++ js/index.js
@@ -5,8 +5,22 @@ function $(id) {
 function showPreview() {
 	let instance = $("instance").value;
 	let toot_id = $("toot-id").value;
-	let embed_url = instance + "/api/v1/statuses/" + toot_id + "/embed";
-	let target_div = $("card-preview")
-	
-	target_div.innerHTML = '<iframe src="' + embed_url + '" class="mastodon-embed" style="max-width: 100%; border: 0" width="400"></iframe>';
+	let toot_url = instance + "/api/v1/statuses/" + toot_id;
+	let target_div = $("card-preview");
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", toot_url, true);
+	xhr.onload = function (e) {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				target_div.innerHTML = '<iframe src="' + JSON.parse(xhr.responseText).uri + '"/embed">';
+			} else {
+				console.error(xhr.statusText);
+			}
+		}
+	};
+	xhr.onerror = function (e) {
+		console.error(xhr.statusText);
+	};
+	xhr.send(null); 
 }
