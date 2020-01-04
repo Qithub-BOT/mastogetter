18e34e7d211840bd47d2957b9da159acd7db0d36
hidao80 <8155294+hidao80@users.noreply.github.com>
2020-01-04 23:27:18 +0900

    feat #9: permalink read to gathering cards list.

--- js/index.js
+++ js/index.js
@@ -60,15 +60,66 @@ function deleteCard(index) {
 	genPermalink();
 }
 
-function genPermalink() {
+function genPermalink(toot_list) {
 	let permalink = "https://hidao80.github.io/mastogetter/p.html?i="+ $("instance").value +"&t=";
-	Object.keys(card_list).forEach(function (key) {
-		permalink += card_list[key] + ",";
-	});
+	if (toot_list === undefind) {
+		Object.keys(card_list).forEach(function (key) {
+			permalink += card_list[key] + ",";
+		});
+	} else {
+		permalink += toot_list;
+	}
 	$("permalink").value = permalink;
 }
 
 function copyPermalink() {
 	$("permalink").select();
 	document.execCommand("copy");
+}
+
+function loadPermalink() {
+	const permalink = $("load").value;
+	const permalink_str = {"i": permalink.split("?i=")[1].split("&")[0],
+						 "t": permalink.split("&t=")[1]};
+	const permalink_obj = decodePermalink(permalink_str);
+	const instance_full = permalink_obj["instance_full"]; 
+	const instance = permalink_obj["instance"];
+	const toot_ids = permalink_obj["toot_ids"];
+	let toot_url = "";
+	const target_div = $("cards");
+	let xhr = new XMLHttpRequest();
+	$("instance").value = instance_full;
+
+	for (let i = 0; i < toot_ids.length; i++) {
+		toot_url = instance_full + "/api/v1/statuses/" + toot_ids[i];
+		xhr.open("GET", toot_url, false);
+		xhr.onload = function (e) {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					let obj = JSON.parse(xhr.responseText);
+					let timestamp = moment(obj.created_at).format('LLLL');
+					let e = document.createElement("div");
+					e.setAttribute("class","toot");
+					let tmp = "";
+					for (let i = 0; i < obj.media_attachments.length; i++) {
+						tmp += "<a href='"+ obj.media_attachments[i].url +"'><img class='thumbs' src='"+ obj.media_attachments[i].preview_url +"'></a>";
+					}
+					e.innerHTML = '<div class="box"><img width="48" height="48" alt="" class="u-photo" src="'+ obj.account.avatar +'"></div>'
+						+ '<div class="box"><span class="display-name">'+ obj.account.display_name
+						+ '<span>@'+ obj.account.username +'@'+ instance +'</span></span>'
+						+ '<span class="create-at">' + timestamp + '</span>'
+						+ '<div class="e-content" lang="ja" style="display: block; direction: ltr"><p>'+ obj.content +'</p></div>'
+						+ tmp + '</div>';
+					target_div.appendChild(e);
+				} else {
+					console.error(xhr.statusText);
+				}
+			}
+		};
+		xhr.onerror = function (e) {
+			console.error(xhr.statusText);
+		};
+		xhr.send(null);
+	}
+	genPermalink(toot_ids.join(","));
 }
